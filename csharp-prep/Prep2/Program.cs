9106ae39227892081781836da357e83ff12ec218
lauralandonlovell <roryboo@gmail.com>
2023-09-14 09:13:54 +0200

    creating second if statement for pass/fail

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -9,36 +9,46 @@ class Program
         int grade = (Convert.ToInt32(gPercentage));
         Console.WriteLine(grade);
 
-        if (grade > 60 && grade <= 100)
-        {  
-            if (grade >= 90)
-            {
-                string hiGrade = "A";
-                Console.WriteLine(hiGrade);
-            }
-            else if (grade >= 80 && grade < 90 )
-            {
-                string letterGrade = "B";
-                Console.WriteLine(letterGrade);
-            }
-            
-            else if (grade >= 70 && grade < 80)
-            {
-                string midgrade = "C";
-                Console.WriteLine(midgrade);
-            }
-            else if (grade >= 60 && grade < 70)
-            {
-                string lowGrade = "D";
-                Console.WriteLine(lowGrade);
-            }
-            Console.WriteLine("Congratulations you passed!");
-        }
-        else if
+        string gradeLetter = "";
+
+        //if (grade > 60 && grade <= 100)
+        //{  
+        if (grade >= 90)
         {
-            string badGrade = "F";
-            Console.WriteLine($"Your grade was {badGrade}. Sorry you didn't pass. Better luck next time.");
-        } 
+            gradeLetter = "A";
+            Console.WriteLine(gradeLetter);
+        }
+        else if (grade >= 80 && grade < 90 )
+        {
+            gradeLetter = "B";
+            Console.WriteLine(gradeLetter);
+        }
+            
+        else if (grade >= 70 && grade < 80)
+        {
+            gradeLetter = "C";
+            Console.WriteLine(gradeLetter);
+        }
+        else if (grade >= 60 && grade < 70)
+        {
+            gradeLetter = "D";
+            Console.WriteLine(gradeLetter);
+        }
+            //Console.WriteLine("Congratulations you passed!");
+        //}
+        else /*if*/
+        {
+            gradeLetter = "F";
+            
+        }
+        if (gradeLetter >= 70) 
+        {
+            Console.WriteLine("Cangratulations! You passed!");
+        }
+        else
+        {
+           Console.WriteLine($@"Sorry you didn't pass. Don't give up! Better luck next time!"); 
+        }
 
 
        
