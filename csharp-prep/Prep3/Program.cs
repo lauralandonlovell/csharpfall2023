76ffee74e1e0822dd1b3a73e295a483a611aa7f6
lauralandonlovell <roryboo@gmail.com>
2023-09-18 15:14:12 +0200

    got input coverted to int

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,31 +1,25 @@
 using System;
+using System.Globalization;
 using System.Security.Cryptography;
 
 class Program
 {
     static void Main(string[] args)
     {
-        //User input variables
+        //Getting user input
         Console.Write("Enter a magic number: ");
         string magicNum = Console.ReadLine();
         Console.Write("Enter a guess: ");
-        string userNum = Console.ReadLine(); 
+        string userNum = Console.ReadLine();
 
-        //Printing to console
-        
-        
-
-        
-        
-
-      
-
-
-         
-        
-
+        //converting data type to int and printing to console to test
+        int magNum = Int32.Parse(magicNum);
+        //Console.WriteLine(magNum); 
+        int guessNum = Int32.Parse(userNum);
+        //Console.WriteLine(guessNum);
 
         
+
 
 
     }
