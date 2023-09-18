2fbff4e91afad9bb26e9dc93e17566676f8f1554
lauralandonlovell <roryboo@gmail.com>
2023-09-18 13:22:29 +0200

    getting user input

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,28 @@
 using System;
+using System.Security.Cryptography;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        //Asking for user input
+        Console.WriteLine("Enter a magic number: ");
+        //Getting user input
+        string magicNum = Console.ReadLine();
+        /*Console.WriteLine(magicNum);
+        int magicNumber = (magicNum);
+        //Asking for user input again
+        Console.WriteLine("Enter a guess: ");
+        //Getting user input 
+        Console.ReadLine();
+
+        //Importing Random class and instantiating
+        //Random random = new Random();
+        //Getting random value within range
+        string inputGuess = 
+
+        Console.WriteLine(number);*/
+
+
     }
 }
