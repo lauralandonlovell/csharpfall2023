ec727bafbf682ba78dc7b4d8fcd60e5f532bc081
lauralandonlovell <roryboo@gmail.com>
2023-09-18 13:56:29 +0200

    puting placeholder input values in variables

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,6 +5,12 @@ class Program
 {
     static void Main(string[] args)
     {
+        //User input
+        Console.Write("Enter a magic number: ");
+        string magicNum = "";
+        Console.Write("Enter a guess: ");
+        string userNum = "";
+
         
         
 
