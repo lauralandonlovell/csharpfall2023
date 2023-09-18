0a771f49c65eb5ed6ab5eedd60c54720a857906c
lauralandonlovell <roryboo@gmail.com>
2023-09-18 13:51:34 +0200

    starting over

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,24 +5,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        //Asking for user input and returning it to console
-        Console.WriteLine("Enter a magic number: ");
-        string magicNum = Console.ReadLine();
-        Console.WriteLine(magicNum);
+        
+        
 
-        Console.WriteLine("Enter a guess: ");
-        string userGuess = Console.ReadLine();
-        Console.WriteLine(userGuess);
+      
+
+
          
         
 
 
-        //Importing Random class and instantiating
-        //Random random = new Random();
-        //Getting random value within range
-        //string inputGuess = 
-
-        //Console.WriteLine(number);
+        
 
 
     }
