f87af537ae5100ea03cc9c78871ed6af8d5c2003
lauralandonlovell <roryboo@gmail.com>
2023-09-18 15:45:45 +0200

    print statement and type casting at once

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -8,17 +8,38 @@ class Program
     {
         //Getting user input
         Console.Write("Enter a magic number: ");
-        string magicNum = Console.ReadLine();
+        int magicNum = Convert.ToInt32(Console.ReadLine());
         Console.Write("Enter a guess: ");
-        string userNum = Console.ReadLine();
+        int userNum = Convert.ToInt32(Console.ReadLine());
+
+        //Console.WriteLine(Double.Parse(magicNum));
+        //Console.WriteLine(Double.Parse(userNum));
 
         //converting data type to int and printing to console to test
-        int magNum = Int32.Parse(magicNum);
-        //Console.WriteLine(magNum); 
-        int guessNum = Int32.Parse(userNum);
+        //int mNum = ;
+        //Console.WriteLine(magicNum); 
+        //int guessNum = Int32.Parse(userNum);
         //Console.WriteLine(guessNum);
 
-        
+        //if statement
+        if(userNum < magicNum)
+        {
+            Console.WriteLine("Guess higher next time!");
+        }
+        else if(userNum > magicNum)
+        {
+            Console.WriteLine("Guess lower next time!");
+        }
+        else if(guessNum == magicNum)
+        {
+            Console.WriteLine("Congratulations, you guessed it!");
+        }
+        else
+        {
+            Console.WriteLine("Sorry, try another number!");
+        }
+
+
 
 
 
