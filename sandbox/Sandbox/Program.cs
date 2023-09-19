8f75ccdd3ad755d465f3be1b7ba0135034e3da66
lauralandonlovell <roryboo@gmail.com>
2023-09-19 08:24:36 +0200

    getting rid of comments.

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,5 +5,43 @@ class Program
     static void Main(string[] args)
     {
        Console.WriteLine("Hello Sandbox World!");
+       Console.Write("Enter a magic number: ");
+       string magicNum = Console.ReadLine();
+       Console.Write("Enter a guess: ");
+       string userNum = Console.ReadLine();
+       int magNum = Convert.ToInt32(magicNum);
+       int guess = Convert.ToInt32(userNum);
+       Console.WriteLine(magNum);
+       Console.WriteLine(guess);
+
+        
+       while (magNum != guess)
+       {
+            
+            if (magNum > guess)
+            {
+                Console.WriteLine("Guess higher!");
+            }
+            else if (magNum < guess)
+            {
+                Console.WriteLine("Guess lower!");
+            }
+        
+            else if(magNum == guess)
+            {
+                Console.WriteLine("Congratulations! You guessed it!");
+            }
+            else
+            {
+                string magic = Convert.ToString(magNum);
+                string userGuess = Convert.ToString(guess);
+                Console.WriteLine("Was that a number? I couldn't tell!");
+                Console.WriteLine(magic);
+                Console.WriteLine(userGuess);
+            
+            }
+        
+        }
     }
+    
 }
