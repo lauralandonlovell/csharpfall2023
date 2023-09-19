8d016e08ac94d2f20dca98deac9f33f633801e56
lauralandonlovell <roryboo@gmail.com>
2023-09-19 08:35:29 +0200

    loop stuck. fixing indentation

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,19 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-       Console.WriteLine("Hello Sandbox World!");
-       Console.Write("Enter a magic number: ");
-       string magicNum = Console.ReadLine();
-       Console.Write("Enter a guess: ");
-       string userNum = Console.ReadLine();
-       int magNum = Convert.ToInt32(magicNum);
-       int guess = Convert.ToInt32(userNum);
-       Console.WriteLine(magNum);
-       Console.WriteLine(guess);
+    
+        
+        Console.WriteLine("Hello Sandbox World!");
+        Console.Write("Enter a magic number: ");
+        string magicNum = Console.ReadLine();
+        Console.Write("Enter a guess: ");
+        string userNum = Console.ReadLine();
+        int magNum = Convert.ToInt32(magicNum);
+        int guess = Convert.ToInt32(userNum);
+        Console.WriteLine(magNum);
+        Console.WriteLine(guess);
 
         
-       while (magNum != guess)
-       {
+        while (magNum != guess)
+        {
             
             if (magNum > guess)
             {
