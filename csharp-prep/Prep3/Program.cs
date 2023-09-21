013b9b58534e6ba6aa4b7389a309bcc54e64000a
lauralandonlovell <roryboo@gmail.com>
2023-09-21 19:15:05 +0200

    not breaking but has a lot of errors

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,51 +5,81 @@ class Program
 {
     static void Main (string[] args)
     {   
-        //No loop yet.
-        Console.Write("Enter a magic number: ");
-        string magicNum = Console.ReadLine();
-        Console.WriteLine(magicNum);
+        /*Input
+          Console.Write("Enter a magic number: ");
+          int magicNum = int.Parse(Console.ReadLine());*/
         
-        
-        /*Console.WriteLine("Guess the magic number in 10 tries or less!\n");
-        // Instantiating new Random object to generate magic number
+            // Instantiating new Random object to generate magic number
             Random rndNumber = new Random();
-            int magNum = rndNumber.Next(1, 100);
-            string magic = Convert.ToString(magNum);
-            List<int> itries = new List<int>();
-            Console.WriteLine("Guess a number: ");
-            string guess = Console.ReadLine();
-            for (int i = 1; i < itries.Length; i++)
+            int magicNum = rndNumber.Next(1, 11);
+            Console.WriteLine(magicNum);
+            //Setting variable to contain input
+            int guess = 0;
+            /*Trying and failing at stretch challenge:(
+               string magic = Convert.ToString(magNum);
+               List<int> itries = new List<int>();
+               Console.WriteLine("Guess a number: ");
+               string guess = Console.ReadLine();*/
+            //Setting variable to contain tries count
+            int tries = -10;
+            //Setting variable to contain iteration count 
+            int i = 1;
+            //chose do-while loop so that incrementation is the same as input frequency
+            do
             {
-                int randomNumber = magicNum.Next(1,100);
-                string magNum = Convert.ToChar(randomNumber).ToString();
-                //test
-                Console.WriteLine(magNum);
-                .Add(); 
-            }
-            foreach(string item in )   
-            {
-                //Getting user input guess
-                Console.Write("Guess a number: ");
-                string guess = Console.ReadLine();
-            }
+                Console.WriteLine("Guess a number: ");
+                guess = int.Parse(Console.ReadLine());
+
+                if (magicNum == guess)
+                {
+                    Console.WriteLine("You guessed it!");
+                    break;
+                }
+                else if (magicNum > guess)
+                {
+                    Console.WriteLine("Guess higher next time!");
+                }
+                else if (magicNum < guess)
+                {
+                    Console.WriteLine("Guess lower next time!");
+                }
+                else
+                {
+                    Console.WriteLine("Was that a number? Try again.");
+                }
+                tries++;
+                i++;
+                Console.WriteLine($@"Only ({tries}-{i}) guess(es) left!");
+            } while (guess <= 10);
+
+                
+
+
+
+
+
+
+
+            // for (int i = 1; i < itries.Length; i++)
+            // {
+            //     int randomNumber = magNum.Next(1,100);
+            //     string magNum = Convert.ToChar(randomNumber).ToString();
+            //     //test
+            //     Console.WriteLine(magNum);
+            //     .Add(); 
+            // }
+            // foreach(string item in )   
+            // {
+            //     //Getting user input guess
+            //     Console.Write("Guess a number: ");
+            //     string guess = Console.ReadLine();
+            // }
             //Conditions
             
-            if (string randoNum > string guess)
-            {
-                Console.WriteLine("Guess higher next time!");
-            }
-            else if (rando < userGuess)
-            {
-                Console.WriteLine("Guess lower next time!");
-            }
-            else if (rando == userGuess)
-            {
-
-            }*/
-        Console.WriteLine("Would you like to play again? Type yes or no: ");
-        response = Console.ReadLine();
-        Console.WriteLine(response);
+            
+        // Console.WriteLine("Would you like to play again? Type yes or no: ");
+        // response = Console.ReadLine();
+        // Console.WriteLine(response);
     }
 }
 
