9e3c7e7501f84c5c0a23bc22c3ffdf0c053aa9ce
lauralandonlovell <roryboo@gmail.com>
2023-09-22 01:14:50 +0200

    creating functions and return value types

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -5,5 +5,31 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep5 World!");
+        PrintWelcome();
+        string person = UserName();
+        int number = UserNumber();
+
+    }
+
+    static void PrintWelcome()
+    {
+        Console.WriteLine("Welcome!!");
+    }
+
+    static string UserName()
+    {
+        Console.Write("Enter your name: ");
+        string name = Console.ReadLine();
+
+        return name;
+    }
+    static double UserNumber()
+    {
+        Console.Write("Enter your favorite number: ");
+        string number = Console.ReadLine();
+        double numBer = Convert.ToDouble(number);
+        
+        return ;
+
     }
 }
