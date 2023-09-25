b7ef2dfabefba377c3cbec950dedde80570d0489
lauralandonlovell <roryboo@gmail.com>
2023-09-25 11:55:55 +0200

    added comment

--- prepare/Learning02/Job.cs
+++ prepare/Learning02/Job.cs
@@ -2,7 +2,8 @@ public class Job
 {
     public string _jobTitle;
     public string _company;
-    public string _startYear;
+
+    public string _startYear;/*Example solution shows data type as int. I was thinking that the year would be taken as input from the user, so that is why I have it as string. I was thinking that I would need to do a data conversion method as one of the class methods to change the year to an int, and if that worked a field that calculated the duration at each job could be an option.*/
     public string _endYear;
 
     public Job()
