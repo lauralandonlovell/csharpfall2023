1086302693697c0c9609cd82d406a5ef65268b63
lauralandonlovell <roryboo@gmail.com>
2023-09-26 10:00:58 +0200

    added signature code to program. user input name.

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -11,6 +11,11 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Welcome to the Journal Program. Enjoy keeping track of your day!");
+        Console.WriteLine();
+        Console.Write("What is your first name? ");
+        string _firstName = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        string _lastName = Console.ReadLine();
         Console.WriteLine("Please choose from the following menu items: ");
 
         Prompts
