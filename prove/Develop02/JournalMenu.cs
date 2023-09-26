22d1b4d8fcb7d4a84b5605336e70d73c6930089c
lauralandonlovell <roryboo@gmail.com>
2023-09-26 10:13:43 +0200

    created JournalMenu class

--- prove/Develop02/JournalMenu.cs
+++ prove/Develop02/JournalMenu.cs
@@ -6,7 +6,9 @@ public class JournalMenu
     public string _saveEntry;
     public string _fetchEntry;
     public string _quitJournal;
-    List<JournalMenu>_choices = new List<JournalMenu>("1. Write", "2. Read", "3. Save", "4. Load", "5. Quit");
+    public JournalMenu()
+    {
+    }
 
     public void Display()
     {
--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -12,10 +12,12 @@ class Program
     {
         Console.WriteLine("Welcome to the Journal Program. Enjoy keeping track of your day!");
         Console.WriteLine();
+        //code for Signature
         Console.Write("What is your first name? ");
         string _firstName = Console.ReadLine();
         Console.Write("What is your last name? ");
         string _lastName = Console.ReadLine();
+        //code for JournalMenu
         Console.WriteLine("Please choose from the following menu items: ");
 
         Prompts
