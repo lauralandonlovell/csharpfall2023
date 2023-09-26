4c82ad37ea6ed6e9ccf69458c93a95e9a5dd5b64
lauralandonlovell <roryboo@gmail.com>
2023-09-26 18:31:05 +0200

    added random, but think I need to move to Entry

--- prove/Develop02/Prompt.cs
+++ prove/Develop02/Prompt.cs
@@ -1,17 +1,19 @@
 using System;
 public class Prompt
 {
-    //Stores list of writing prompts; returns a prompt
-    public List<string> _prompts = new List<string>();
+    //Stores list of writing prompts; returns a random prompt.
+    string[] _prompts = {"Today was wonderful because...","Today I felt the Spirit with me when...","I enjoyed helping when I...", "Today I was grateful for...", "Today I had a really good day because...", "One challenge I overcame today was...", "I really like myself because..."};
     public Prompt()
     {
     }
-    public void Display()
+    public 
     {
-        foreach(Prompt prompt in _prompts)
         {
-            Random prompt = new Random();
-            Console.WriteLine(prompt);
+            //Generating a random index for _prompts array. Found info on Random() at https://learn.microsoft.com/en-us/dotnet/api/system.random?view=net-7.0
+            Random _entryPrompt = new Random();
+            int _promptIndex = _entryPrompt.Next(_prompts.Length);
+            //Displaying and returning random prompt to other classes.
+            
         }
       
     }
