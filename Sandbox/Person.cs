4b45fa78e5c8a110b6da97a871ee0ead43166a1e
lauralandonlovell <roryboo@gmail.com>
2023-10-04 12:14:35 +0200

    fixing errors; was lacking top level statements

--- Sandbox/Person.cs
+++ Sandbox/Person.cs
@@ -1,6 +1,15 @@
+using System;
+
 public class Person
 {
    public string _firstName;
    public string _lastName;
    public int _age;
+}
+Person person
+{
+}
+ void Display()
+{
+   Console.WriteLine(person)
 }
--- Sandbox/Program.cs
+++ Sandbox/Program.cs
@@ -1,11 +1,27 @@
 using System;
-using System.Security.Cryptography.X509Certificates;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
         Person p1 = new Person();
+        p1._firstName = "Mary";
+        p1._lastName = "Smith";
+        p1._age = 25;
+
+        Person p2 = new Person();
+        p2._firstName = "John";
+        p2._lastName = "Watkins";
+        p2._age = 30;
+
+        List<Person> people = new List<Person>();
+        people.Add(p1);
+        people.Add(p2);
+
+
+
+
                     
     }
 }    
