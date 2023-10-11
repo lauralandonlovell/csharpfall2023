2676e7b44fa891839d76fbd10b42190e12e365ca
lauralandonlovell <roryboo@gmail.com>
2023-10-11 12:23:04 +0200

    wrking on not depending on accessors

--- prove/Develop02/Prompt.cs
+++ prove/Develop02/Prompt.cs
@@ -1,2 +1,12 @@
 using System;
 public class Prompt
+{
+    private List<string> _prompts = new List<string>(){"Today is a great day because...", "My prayers were answered today when...", "I know God loves me because...", "Today was fun because...", "I am going to graduate in...", "I picked applied technology for my major because..."};
+
+    public void Display();
+    {
+        forEach(string Prompt prompt in _prompts);
+        _prompts.Add(prompt);
+        Console.WriteLine(prompt);
+    }
+}
