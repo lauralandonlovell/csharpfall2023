1008b0615f7cbbd400cec303729100413b12448f
lauralandonlovell <roryboo@gmail.com>
2023-10-11 12:24:54 +0200

    made List public so entry class can have it as arg

--- prove/Develop02/Prompt.cs
+++ prove/Develop02/Prompt.cs
@@ -1,7 +1,7 @@
 using System;
 public class Prompt
 {
-    private List<string> _prompts = new List<string>(){"Today is a great day because...", "My prayers were answered today when...", "I know God loves me because...", "Today was fun because...", "I am going to graduate in...", "I picked applied technology for my major because..."};
+    public List<string> _prompts = new List<string>(){"Today is a great day because...", "My prayers were answered today when...", "I know God loves me because...", "Today was fun because...", "I am going to graduate in...", "I picked applied technology for my major because..."};
 
     public void Display();
     {
