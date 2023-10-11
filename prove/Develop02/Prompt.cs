72f581c7ced7e4d241f778040ff6870789799f94
lauralandonlovell <roryboo@gmail.com>
2023-10-11 13:03:28 +0200

    redoing Prompt class again

--- prove/Develop02/Prompt.cs
+++ prove/Develop02/Prompt.cs
@@ -1,12 +1,1 @@
 using System;
-public class Prompt
-{
-    public List<string> _prompts = new List<string>(){"Today is a great day because...", "My prayers were answered today when...", "I know God loves me because...", "Today was fun because...", "I am going to graduate in...", "I picked applied technology for my major because..."};
-
-    public void Display();
-    {
-        forEach(string Prompt prompt in _prompts);
-        _prompts.Add(prompt);
-        Console.WriteLine(prompt);
-    }
-}
